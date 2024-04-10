de85b4aa307cca3ca1927025a65f8dd83fe37ffa
Paisit Jarunnamsiri <paisitj@Paisits-MacBook-Pro-14.local>
2024-04-10 13:14:42 -0700

    ch02

---
+++ ch02/listing2-3/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func doWork(id int) {
+	fmt.Printf("Work %d started at %s\n", id, time.Now().Format("15:04:05"))
+	time.Sleep(time.Second)
+	fmt.Printf("Work %d finished at %s\n", id, time.Now().Format("15:04:05"))
+}
+
+func main() {
+	for i := 0; i < 5; i++ {
+		go doWork(i)
+	}
+	time.Sleep(time.Second * 2)
+}
---
+++ ch02/listing2-4/cpucheck.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	fmt.Println("Number of CPUs:", runtime.NumCPU())
+	fmt.Println("GOMAXPROCS:", runtime.GOMAXPROCS(0))
+}
